4f1d749524ee4a4ec5e7bac71b2a05067f875774
yichunzhao <zhaoyichun@hotmail.com>
2020-07-20 13:34:47 +0200

    updating comment

--- src/main/java/com/ynz/explorelombok/data/DemoDataBuilder.java
+++ src/main/java/com/ynz/explorelombok/data/DemoDataBuilder.java
@@ -5,9 +5,10 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * If using Lombok @Data and @Builder together, all-args constructor(package level) is generated.
+ * If using Lombok @Data and @Builder together, all-args constructor(package level) is generated. however, the
+ * non-argument constructor will be removed. For a POJO, it may cause a reflection error.
  *
- * Without adding AllArgument annotation.
+ * In a personal opinion, it is not a good idea to apply lombok on the entity besides getters and setters.
  */
 
 @Data
@@ -22,5 +23,7 @@ public class DemoDataBuilder {
     public static void main(String[] args) {
         Dog dog = Dog.builder().name("dou").weight(7).build();
         log.info(dog.toString());
+
+        //new Dog();
     }
 }
